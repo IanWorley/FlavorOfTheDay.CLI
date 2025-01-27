c7a3d77a9c8f00427ea672acb33a847c586650c8
Ian Worley <ian@ianworley.xyz>
2025-01-27 14:13:33 -0600

    feat: needed to add project files

---
+++ Tests/Service/TestFlavorOfTheDayService.cs
@@ -0,0 +1,53 @@
+﻿using Business;
+using Moq;
+using Moq.Protected;
+using Tests.Utils;
+
+namespace Tests.Service;
+
+public class TestFlavorOfTheDayService
+{
+    [Fact]
+    public async Task GetFlavorOfTheDayAsync_ReturnsExpectedResult()
+    {
+        //Arrange 
+        var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        var expectedResponse = CommonMocks.WorkingMockedJsonResponse();
+        mockHttpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>(
+            "SendAsync",
+            ItExpr.IsAny<HttpRequestMessage>(),
+            ItExpr.IsAny<CancellationToken>()
+        ).ReturnsAsync(expectedResponse);
+
+        var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        httpClientFactoryMock.Setup(_ => _.CreateClient("FavorOfTheDayByZipLimit")).Returns(httpClient);
+        var service = new FlavorOfTheDayService(httpClientFactoryMock.Object);
+
+        var result = await service.GetFlavorOfTheDayAsync(12345);
+
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.Equal("123 Main St, Springfield, IL, 62701", result.Keys.First());
+    }
+
+    [Fact]
+    public async Task ValidateArgumentsForLimits()
+    {
+        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        var expectedResponse = CommonMocks.WorkingMockedJsonResponse();
+
+        mockHttpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>(
+            "SendAsync",
+            ItExpr.IsAny<HttpRequestMessage>(),
+            ItExpr.IsAny<CancellationToken>()
+        ).ReturnsAsync(expectedResponse);
+
+        await Assert.ThrowsAsync<ArgumentException>(() =>
+            new FlavorOfTheDayService(httpClientFactoryMock.Object).GetFlavorOfTheDayAsync(12345, -1));
+
+    }
+
+
+}
